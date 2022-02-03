95edfeaf4ece136c4d9864c54af47147cf42ef68
Vítor Del' Duca <vitordelduca@hotmail.com>
2022-02-03 17:55:55 -0300

    Inclusão dos Arquivos
    Inclusão:
    - Arquivo HTML;
    - Arquivo CSS;
    - Arquivo JS;
    - Arquivo Imagens

---
+++ JS/index.js
@@ -0,0 +1,39 @@
+let queryKey = "api_key=FIiBibpW5TwIT2xgX70x5KXIkdvYX6VCf3TGcByq"
+let queryURL = `https://api.nasa.gov/planetary/apod?${queryKey}`
+console.log(queryURL)
+
+
+
+$('#enviar').on('click', requiApi)
+
+function requiApi () {
+    const dataApi = $('#datas').val()
+
+    $.ajax({url: queryURL + '&date=' + dataApi, 
+        success: function (retorno) {
+            return enviaApi(retorno)
+        }
+    })
+}
+
+function enviaApi (obj) {
+    let imagens = obj.url
+    let tipo = JSON.stringify (obj.media_type)
+
+    $('#data').html(` Data ${obj.date}`)
+    $('#autoria').html(` Créditos da Imagem & Copyright: ${obj.copyright}`)
+    $('#legenda').html(obj.explanation)
+
+    if (tipo === '"image"') {
+        $('#imagem').css('display', 'block')
+        $('#video').css('display', 'none')
+        $('#img').attr('src', imagens)
+        
+    } else {
+        $('#video').css('display', 'block')
+        $('#imagem').css('display', 'none')
+        $('#vid').attr('src', imagens)
+    
+    }  
+    
+}
